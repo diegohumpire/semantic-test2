4a9786be5ec054dda10ac8c65d0cd7af2cbbe84a
Diego Humpire <dh@mambo.pe>
2016-03-04 16:34:19 -0500

    Migration to Jade. Integration with Vue.js

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({defaultLayout: 'layout', extname: '.hbs'}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'jade');
+// app.engine('.jade', exphbs({ defaultLayout: 'layout', extname: '.jade' }));
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-  $('.ui.dropdown').dropdown();
+var todos = [
+  { task: 'Task #1', state: false, text: 'Descripcion de la Tarea' },
+  { task: 'Task #2', state: false, text: 'Descripcion de la Tarea' },
+  { task: 'Task #3', state: true, text: 'Descripcion de la Tarea' },
+  { task: 'Task #4', state: false, text: 'Descripcion de la Tarea' }
+];
+
+new Vue({
+  el: '#app',
+  data: {
+    todos: todos
+  }
 });
