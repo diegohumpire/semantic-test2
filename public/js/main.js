bbb0894e1cc04cc042913110ac575e1707110db8
Diego Humpire <dh@mambo.pe>
2016-03-22 18:09:19 -0500

    Add comments on javascript code

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,43 @@
+// Todos
 var todosList = [
-  { id:'1', title: 'Task #1', state: false, description: 'Descripcion de la Tarea' },
-  { id:'2', title: 'Task #2', state: false, description: 'Descripcion de la Tarea' },
-  { id:'3', title: 'Task #3', state: true, description: 'Descripcion de la Tarea' },
-  { id:'4', title: 'Task #4', state: false, description: 'Descripcion de la Tarea' }
+  { id: '1', title: 'Task #1', state: false, description: 'Descripcion de la Tarea' },
+  { id: '2', title: 'Task #2', state: false, description: 'Descripcion de la Tarea' },
+  { id: '3', title: 'Task #3', state: true, description: 'Descripcion de la Tarea' },
+  { id: '4', title: 'Task #4', state: false, description: 'Descripcion de la Tarea' }
 ];
 
+// Todo Constructor
 var newTodoObj = function(props) {
+
+  // New object
   var todo = props || {};
+
   this.id = todo.id || 0;
   this.title = todo.title || '';
   this.state = todo.state || false;
   this.description = todo.description || '';
 };
 
+// Todo finder
 var getById = function(arrayObj, id) {
+
+  // filtering by id
   var objFilter = arrayObj.filter(function(obj) {
     return obj.id == id;
   });
+
+  // first object
   var index = arrayObj.indexOf(objFilter[0]);
-  
+
   return index;
 };
 
+// if exists the object
 var isEmpty = function(obj) {
   return obj.id === 0 ? true : false;
 };
 
+// Vue Object
 var vm = new Vue({
   el: '#app',
   data: {
@@ -33,6 +45,10 @@ var vm = new Vue({
     todo: new newTodoObj()
   },
   computed: {
+    /**
+     * Return a object with the valid state of properties 
+     * by default returns true, after a model's property is touched, change the valid state
+     */
     validation: function() {
       return {
         title: this.$validationTodo.title.touched ? this.$validationTodo.title.valid : true,
@@ -56,22 +72,37 @@ var vm = new Vue({
       this.todo = new newTodoObj();
     },
     saveTodo: function(todo) {
-      var isValid = this.$validationTodo.valid;
       
+      // Valid state from vue-validation
+      var isValid = this.$validationTodo.valid;
+
       if (isValid) {
-        if(todo.id === 0) {
+        
+        // If todo is new. TODO: Other way to check this. 
+        if (todo.id === 0) {
+          
+          // Simulate the new autoincrement ID
           todo.id = this.todos.length + 1;
           this.todos.push(todo);
+          
         } else {
+          
+          // Find the todo in the array
           var index = getById(this.todos, todo.id);
           this.todos.$set(index, todo);
         }
-        this.todo = new newTodoObj();  
+        
+        // Reset todo's model
+        this.todo = new newTodoObj();
+        
+        // Reset validation
         this.$resetValidation();
       }
-      
+
     },
     removeTodo: function(todo) {
+      
+      // vue's way to remove obj from array
       this.todos.$remove(todo);
     }
   }
