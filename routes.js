95f956c1a5cb1c02dcf73aae17aef7c25e802c8d
Diego Humpire <dh@mambo.pe>
2016-04-26 12:41:05 -0500

    send message fb bot

--- routes.js
+++ routes.js
@@ -23,6 +23,29 @@ var routesGlobal = function(app) {
     res.send('Error, wrong validation token');
   });
   
+  var token = "EAAWv3GcO0moBAJ7MUGKIchQDr1ZCi1bZCOTyZAmOHBVidzNXxeWaic7ULLkeLZASq0C5YMlyqCVVcQaU63fqKAIv6dqYoP3iLzu9hut2kbV8eZBAXEVOoFQZBfrYQcw8ISkEY7NmA1LXpOCnAj88yj9kAhxwyZBq8iZCjE9nhonRhgZDZD";
+
+  function sendTextMessage(sender, text) {
+    messageData = {
+      text:text
+    }
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token:token},
+      method: 'POST',
+      json: {
+        recipient: {id:sender},
+        message: messageData,
+      }
+    }, function(error, response, body) {
+      if (error) {
+        console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+      }
+    });
+  }
+  
   app.post('/webhook/', function (req, res) {
     messaging_events = req.body.entry[0].messaging;
     for (i = 0; i < messaging_events.length; i++) {
@@ -30,7 +53,7 @@ var routesGlobal = function(app) {
       sender = event.sender.id;
       if (event.message && event.message.text) {
         text = event.message.text;
-        // Handle a text message from this sender
+        sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
       }
     }
     res.sendStatus(200);
