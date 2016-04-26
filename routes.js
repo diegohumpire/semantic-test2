6f3180be8ec88a2649284247fa8fefbd19e3293d
Diego Humpire <dh@mambo.pe>
2016-04-26 11:57:51 -0500

    fb bot messenger test token

--- routes.js
+++ routes.js
@@ -12,6 +12,16 @@ var routesGlobal = function(app) {
   app.get('/pub', function(req, res) {
     console.log('got the get!');    
   });
+  
+  /**
+   * FB Bot Messenger Test
+   */
+  app.get('/webhook/', function(req, res) {
+    if (req.query['hub.verify_token'] === 'my_token_test_ltry') {
+      res.send(req.query['hub.challenge']);
+    }
+    res.send('Error, wrong validation token');
+  });
 };
 
 module.exports = routesGlobal;
