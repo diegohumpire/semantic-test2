e68a63533895f6cef8a58bcddc6c6a94f792e6f0
Diego Humpire <dh@mambo.pe>
2016-04-28 14:50:36 -0500

    refactor fb_bot controller

--- controllers/fb_bot.js
+++ controllers/fb_bot.js
@@ -1,186 +1,8 @@
 var winston = require('winston');
 var request = require('request');
 var format = require('string-format');
+var fb_utils = require('../fb_utils/message');
 var token = "EAAWv3GcO0moBANpu6ZB2vC3zDiyb6Qyi8CY5ulw09vFFueBsW7nkkUQOBXqXId0ewFW2UGhgGtv0ZCMz2WRyFEMFvpeSvVoqYQwRKLVlHOGZCC0OvscA8jI362SSHHkkgh2ZBSdDBP7uYXKdLwr08F8EcCRQss2pc3wKuZCKzGAZDZD";
-
-
-var parserButtons = function(data, type_button) {
-  
-  var type_button = type_button || 'postback';
-  var arrayData = [];
-  
-  try {
-    
-    for(var index in data) {
-      if (index != 3) {
-        var task = data[index];
-        var buttonTemplate = {
-          'type': type_button,
-          'title': task.description,
-          'payload': task.id
-        }
-        arrayData.push(buttonTemplate);
-      }
-    }
-    
-    winston.log('info', 'parserButtons: ' + arrayData.length);
-    
-    return arrayData;
-    
-  } catch(e) {
-    winston.log('info', 'parserButtons: ' + e);
-  }
-};
-
-
-var createMessageData = function(data) {
-  
-  var messageData = {
-    'attachment': {
-      'type': 'template',
-      'payload': {
-        'template_type': 'button',
-        'text': 'Que tarea deseas visualizar?',
-        'buttons': parserButtons(data)
-      }
-    }
-  }
-  
-  return messageData;
-};
-
-
-var sendMessage = function(sender, messageData) {
-  
-  var messageData = createMessageData(messageData);
-  
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token:token },
-    method: 'POST',
-    json: {
-      recipient: { id:sender },
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      winston.log('info', 'Error sending message: ' + error);
-    } else if (response.body.error) {
-      winston.log('info', 'Error - Here!! : ' + response.body.error);
-      var keys = Object.keys(response.body.error);
-      winston.log('info', 'Error - Here!! : ' + keys);
-      winston.log('info', 'Error - Here!! : ' + response.body.error.message);
-      winston.log('info', 'Error - Here!! : ' + response.body.error.type);
-      winston.log('info', 'Error - Here!! : ' + response.body.error.code);
-      winston.log('info', 'Error - Here!! : ' + response.body.error.fbtrace_id);
-    }
-  });
-};
-
-
-var sendGenericMessage = function(sender) {
-  
-  var messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [
-          {
-            "title": "First card",
-            "subtitle": "Element #1 of an hscroll",
-            "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-            "buttons": [
-              {
-                "type": "web_url",
-                "url": "https://www.messenger.com/",
-                "title": "Web url"
-              },{
-                "type": "postback",
-                "title": "Postback",
-                "payload": "Payload for first element in a generic bubble",
-              }
-            ],
-         },{
-            "title": "Second card",
-            "subtitle": "Element #2 of an hscroll",
-            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-            "buttons": [
-              {
-                "type": "web_url",
-                "url": "https://www.messenger.com/",
-                "title": "Web url"
-              },{
-              "type": "postback",
-              "title": "Postback",
-              "payload": "Payload for second element in a generic bubble",
-              }
-            ]
-        },{
-            "title": "Second card",
-            "subtitle": "Element #2 of an hscroll",
-            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Postback",
-                "payload": "Payload for third element in a generic bubble"
-              }
-            ]
-        },{
-            "title": "Second card",
-            "subtitle": "Element #2 of an hscroll",
-            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-            "buttons": [{
-              "type": "postback",
-              "title": "Postback",
-              "payload": "Payload for third element in a generic bubble"
-            }]
-        }]
-      }
-    }
-  };
-  
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token:token },
-    method: 'POST',
-    json: {
-      recipient: { id:sender },
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      winston.log('info', 'Error sending message: ' + error);
-    } else if (response.body.error) {
-      winston.log('info', 'Error: ' + response.body.error);
-    }
-  });
-}
-
-var sendSimpleMessage = function(sender, text) {
-  
-  var messageData = {
-    text: text
-  }
-  
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token:token },
-    method: 'POST',
-    json: {
-      recipient: { id:sender },
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      winston.log('info', 'Error sending message: ' + error);
-    } else if (response.body.error) {
-      winston.log('info', 'Error: ' + response.body.error);
-    }
-  });
-  
-}
 
 
 // Public Actions
@@ -191,20 +13,17 @@ exports.webhook = function (req, res) {
   
   for (i = 0; i < messaging_events.length; i++) {
     
-    // Entrada de FB
+    // Entry FB
     var event = req.body.entry[0].messaging[i];
     
-    // ID de FB de la persona con quien se esta conversado por chat
+    // ID de FB Chat
     var sender = event.sender.id; 
     
-    // winston.log('info', event);
-    // winston.log('info', event.sender);
-    
+    // Message Event
     if (event.message && event.message.text) {
       
-      // Texto del chat
+      // Chat Text
       var text = String(event.message.text);
-      // winston.log('info', text);
       
       try {
         
@@ -213,17 +32,15 @@ exports.webhook = function (req, res) {
           var cmd_text = text.replace(cmd_text_init, '');
           
           if (cmd_text.indexOf(':') == -1) {
-            sendSimpleMessage(sender, 'Por favor ingrese los parametros adecuados');
+            fb_utils.sendSimpleMessage(sender, 'Por favor ingrese los parametros adecuados');
             continue;
           } 
           
           var cmd_specific = cmd_text.substring(0, cmd_text.indexOf(':'));
-          
           // Ej: auth:dhumpire@dhumpire
-          // sendSimpleMessage(sender, format('Se ha enviado un comando - completo "{0}"', cmd_text));
-          
+          // fb_utils.sendSimpleMessage(sender, format('Se ha enviado un comando - completo "{0}"', cmd_text));
           // Ej: auth
-          // sendSimpleMessage(sender, format('Se ha enviado un comando - especifico "{0}"', cmd_specific));
+          // fb_utils.sendSimpleMessage(sender, format('Se ha enviado un comando - especifico "{0}"', cmd_specific));
           
           if (cmd_specific === 'auth') {
             
@@ -232,7 +49,8 @@ exports.webhook = function (req, res) {
             var password = auth_str.substring(auth_str.indexOf('@') + 1, auth_str.length);
             var user_information = {};
             
-            // sendSimpleMessage(sender, format('username: {0} password: {1}', username, password));
+            // fb_utils.sendSimpleMessage(sender, format('username: {0} password: {1}', username, password));
+            
             request({ 
               url: 'https://todo-backend-dj.herokuapp.com/api/v2/api-auth/tokenizer/', 
               method: 'POST', 
@@ -242,9 +60,8 @@ exports.webhook = function (req, res) {
               } 
             }, 
             function(error, response, body) { 
-              // winston.log('info', body);
               user_information = body;
-              sendMessage(sender, user_information.user.tasks);
+              fb_utils.sendMessage(sender, user_information.user.tasks, token);
             });
             continue;
           }
@@ -253,11 +70,11 @@ exports.webhook = function (req, res) {
         }
         
         if (text === 'Generic') {
-          sendGenericMessage(sender);
+          fb_utils.sendGenericMessage(sender);
           continue;
         }
         
-        sendSimpleMessage(sender, text);
+        fb_utils.sendSimpleMessage(sender, text);
         
       }
       catch (e) {
@@ -265,10 +82,11 @@ exports.webhook = function (req, res) {
       }
     }
     
+    // Postback Event
     if (event.postback) {
       text = JSON.stringify(event.postback);
       winston.log('info', text);
-      sendGenericMessage(sender);
+      fb_utils.sendGenericMessage(sender, token);
       continue;
     }
   }
---
+++ fb_utils/message.js
@@ -0,0 +1,198 @@
+var winston = require('winston');
+var request = require('request');
+var format = require('string-format');
+
+/**
+ * FB Messenger Platform just admit 3 buttons per message
+ */
+var parserButtons = function(data, type_button) {
+  
+  var type_button = type_button || 'postback';
+  var arrayData = [];
+  
+  try {
+    
+    for(var index in data) {
+      // Max 3 buttons
+      if (index != 3) {
+        var task = data[index];
+        var buttonTemplate = {
+          'type': type_button,
+          'title': task.description,
+          'payload': task.id
+        }
+        arrayData.push(buttonTemplate);
+      }
+    }
+    
+    winston.log('info', 'parserButtons: ' + arrayData.length);
+    
+    return arrayData;
+    
+  } catch(e) {
+    winston.log('info', 'parserButtons: ' + e);
+  }
+};
+
+
+var createButtonMessageData = function(data) {
+  
+  var messageData = {
+    'attachment': {
+      'type': 'template',
+      'payload': {
+        'template_type': 'button',
+        'text': 'Que tarea deseas visualizar?',
+        'buttons': parserButtons(data)
+      }
+    }
+  }
+  
+  return messageData;
+};
+
+
+exports.sendMessage = function(sender, messageData, token) {
+  
+  var messageData = createButtonMessageData(messageData);
+  
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { 
+      access_token: token 
+    },
+    method: 'POST',
+    json: {
+      recipient: { 
+        id: sender 
+      },
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      winston.log('info', 'Error sending message: ' + error);
+    } else if (response.body.error) {
+      winston.log('info', 'Error - Here!! : ' + response.body.error);
+      var keys = Object.keys(response.body.error);
+      winston.log('info', 'Error - Here!! : ' + keys);
+      winston.log('info', 'Error - Here!! : ' + response.body.error.message);
+      winston.log('info', 'Error - Here!! : ' + response.body.error.type);
+      winston.log('info', 'Error - Here!! : ' + response.body.error.code);
+      winston.log('info', 'Error - Here!! : ' + response.body.error.fbtrace_id);
+    }
+  });
+};
+
+
+exports.sendSimpleMessage = function(sender, text, token) {
+  
+  var messageData = {
+    text: text
+  }
+  
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { 
+      access_token: token 
+    },
+    method: 'POST',
+    json: {
+      recipient: { 
+        id: sender 
+      },
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      winston.log('info', 'Error sending message: ' + error);
+    } else if (response.body.error) {
+      winston.log('info', 'Error: ' + response.body.error);
+    }
+  });
+  
+}
+
+
+exports.sendGenericMessage = function(sender, token) {
+  
+  var messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [
+          {
+            "title": "First card",
+            "subtitle": "Element #1 of an hscroll",
+            "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+            "buttons": [
+              {
+                "type": "web_url",
+                "url": "https://www.messenger.com/",
+                "title": "Web url"
+              },{
+                "type": "postback",
+                "title": "Postback",
+                "payload": "Payload for first element in a generic bubble",
+              }
+            ],
+         },{
+            "title": "Second card",
+            "subtitle": "Element #2 of an hscroll",
+            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+            "buttons": [
+              {
+                "type": "web_url",
+                "url": "https://www.messenger.com/",
+                "title": "Web url"
+              },{
+              "type": "postback",
+              "title": "Postback",
+              "payload": "Payload for second element in a generic bubble",
+              }
+            ]
+        },{
+            "title": "Second card",
+            "subtitle": "Element #2 of an hscroll",
+            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Postback",
+                "payload": "Payload for third element in a generic bubble"
+              }
+            ]
+        },{
+            "title": "Second card",
+            "subtitle": "Element #2 of an hscroll",
+            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+            "buttons": [{
+              "type": "postback",
+              "title": "Postback",
+              "payload": "Payload for third element in a generic bubble"
+            }]
+        }]
+      }
+    }
+  };
+  
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { 
+      access_token: token 
+    },
+    method: 'POST',
+    json: {
+      recipient: { 
+        id: sender 
+      },
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      winston.log('info', 'Error sending message: ' + error);
+    } else if (response.body.error) {
+      winston.log('info', 'Error: ' + response.body.error);
+    }
+  });
+}
