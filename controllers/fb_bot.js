18536ccac2fc4c7cdb45f60b79bb05c9bdf5a393
Diego Humpire <dh@mambo.pe>
2016-04-28 14:33:11 -0500

    test postback

--- controllers/fb_bot.js
+++ controllers/fb_bot.js
@@ -85,41 +85,57 @@ var sendGenericMessage = function(sender) {
       "type": "template",
       "payload": {
         "template_type": "generic",
-        "elements": [{
-          "title": "First card",
-          "subtitle": "Element #1 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://www.messenger.com/",
-            "title": "Web url"
-          },{
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for first element in a generic bubble",
-          }],
-        },{
-          "title": "Second card",
-          "subtitle": "Element #2 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://www.messenger.com/",
-            "title": "Web url"
-          },{
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for second element in a generic bubble",
-          }]
+        "elements": [
+          {
+            "title": "First card",
+            "subtitle": "Element #1 of an hscroll",
+            "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+            "buttons": [
+              {
+                "type": "web_url",
+                "url": "https://www.messenger.com/",
+                "title": "Web url"
+              },{
+                "type": "postback",
+                "title": "Postback",
+                "payload": "Payload for first element in a generic bubble",
+              }
+            ],
          },{
-          "title": "Second card",
-          "subtitle": "Element #2 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-          "buttons": [{
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for third element in a generic bubble"
-          }]
+            "title": "Second card",
+            "subtitle": "Element #2 of an hscroll",
+            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+            "buttons": [
+              {
+                "type": "web_url",
+                "url": "https://www.messenger.com/",
+                "title": "Web url"
+              },{
+              "type": "postback",
+              "title": "Postback",
+              "payload": "Payload for second element in a generic bubble",
+              }
+            ]
+        },{
+            "title": "Second card",
+            "subtitle": "Element #2 of an hscroll",
+            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Postback",
+                "payload": "Payload for third element in a generic bubble"
+              }
+            ]
+        },{
+            "title": "Second card",
+            "subtitle": "Element #2 of an hscroll",
+            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+            "buttons": [{
+              "type": "postback",
+              "title": "Postback",
+              "payload": "Payload for third element in a generic bubble"
+            }]
         }]
       }
     }
