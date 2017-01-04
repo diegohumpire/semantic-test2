8a4636bdcb401d62e1d67fe24a7bc76e18290452
Diego Humpire <dh@mambo.pe>
2017-01-04 16:57:04 -0500

    Add content-type to apple file

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,4 @@
 exports.index = function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
     res.send('{"applinks":{"apps":[],"details":[{"appID":"2H7JJNVPU3.com.mambo.sura.Ahora","paths":["*"]}]},"webcredentials":{"apps":["2H7JJNVPU3.com.mambo.sura.Ahora"]}}');
 };
