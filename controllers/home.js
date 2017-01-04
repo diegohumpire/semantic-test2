f24f07fe5d0fff89374a0542d589d0f8d87d85bb
Diego Humpire <dh@mambo.pe>
2017-01-04 16:38:53 -0500

    Test mobile iOS json

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,3 @@
 exports.index = function(req, res, next) {
-    res.render('index', { title: 'Bienvenido' });
+    res.send('{"applinks":{"apps":[],"details":[{"appID":"2H7JJNVPU3.com.mambo.sura.Ahora","paths":["*"]}]},"webcredentials":{"apps":["2H7JJNVPU3.com.mambo.sura.Ahora"]}}');
 };
