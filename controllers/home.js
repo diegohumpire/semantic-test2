b9b0ad63b1bb3b3a91dddd6e4785b66684244a9f
Diego Humpire <dh@mambo.com>
2017-01-25 20:17:11 -0500

    Test onesingla

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,3 @@
 exports.index = function(req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send('{"applinks":{"apps":[],"details":[{"appID":"2H7JJNVPU3.com.mambo.sura.Ahora","paths":["*"]}]},"webcredentials":{"apps":["2H7JJNVPU3.com.mambo.sura.Ahora"]}}');
+    res.render('index', { title: 'Home' });
 };
